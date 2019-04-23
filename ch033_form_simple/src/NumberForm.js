2e6574194ff321d237ed5fe975327169b236de65
gyujin choi <gyujin0450@naver.com>
2019-04-23 21:07:26 +0900

    3.2.2 반영

---
+++ ch033_form_simple/src/NumberForm.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+
+// 숫자 입력 컴포넌트
+export default class NumberForm extends Component {
+  constructor (props) {
+    super(props)
+    this.state = { value: '' }
+  }
+  // 값이 변경됐을 때
+  doChange (e) {
+    const curValue = e.target.value
+    // 숫자 이외의 값을 제거함
+    const newValue = curValue.replace(/[^0-9]/g, '')
+    this.setState({ value: newValue })
+  }
+
+  // 전송 버튼을 눌렀을때
+  dosubmit (e) {
+    window.alert('전송: ' + this.state.value)
+    e.preventDefault()
+  }
+
+  // 화면 렌더링
+  render () {
+    // 이벤트를 메서드에 바인딩함
+    const doSubmit = (e) => this.doSubmit(e)
+    const doChange = (e) => this.doChange(e)
+    return (
+      <form onSubmit={doSubmit}>
+        <input type='text' value={this.state.value} onChange={doChange} />
+        <input type='submit' value='전송' />
+      </form>
+    )
+  }
+}
--- ch033_form_simple/src/index.js
+++ ch033_form_simple/src/index.js
@@ -1,9 +1,24 @@
+// 3.2.1 예제
+
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import App from './App'
+// import './index.css'
+
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('root')
+// )
+
+// 3.2.2 예제
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
-import './index.css'
+import NumberForm from './NumberForm'
 
+const st = { textAlign: 'center' }
 ReactDOM.render(
-  <App />,
+  <div style={st}>
+    <NumberForm />
+  </div>,
   document.getElementById('root')
 )
